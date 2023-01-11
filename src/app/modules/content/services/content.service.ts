8330912844647295569cb9bcba1c44a12d920898
andijayawizard <andijayawizard@gmail.com>
2023-01-11 18:16:07 +0700

    1101

--- src/app/modules/content/services/content.service.ts
+++ src/app/modules/content/services/content.service.ts
@@ -1,12 +1,23 @@
+import { TypeJobListingFields } from './../content-types';
+import { environment } from './../../../../environments/environment';
 import { createClient } from 'contentful';
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
   client = createClient({
-
+    space: environment.contentful.spaceId,
+    accessToken: environment.contentful.accessToken
   })
   constructor() { }
+  getJobListing(query?: object) {
+    return from(
+      this.client.getEntries<TypeJobListingFields>(Object.assign({
+        content_type: 'jobListing'
+      },query))
+    )
+  }
 }
